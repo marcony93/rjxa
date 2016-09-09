340f2de46c3aeddd815cc474d5ecacb0136e4a35
marcony meza <marcony meza>
2016-09-09 10:14:23 -0600

    agregado bottstrap

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../resources/assets/css/bootstrap.min.css"></link>
+        <script src="../resources/assets/js/jquery-3.1.0.min.js"></script>
+        <script src="../resources/assets/js/bootstrap.min.js"></script>
+        <title>Nosotros</title>
+    </head>
+    <body>
+        @section('menu')
+        <h1>hola meniu</h1>
+        @show
+        @yield('content')
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('nosotros',function(){
+    return view('nosotros');
+});
+
 Route::get('/prueba',function(){
     return "esto es un mensaje de prueba";
 });
