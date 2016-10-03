7f24b8f8ea14988699a6b13959c9218cce5b41ba
Erica Argueta <ericaargueta_11@hotmail.com>
2016-10-02 20:48:21 -0600

    Agregando cambios a conocenos y voluntariado

--- resources/views/conocenos.blade.php
+++ resources/views/conocenos.blade.php
@@ -1,1 +1,35 @@
-Hola
+@extends('layouts.menu')
+@section('menu')
+@parent
+<img class="imgCompleta wow slideInUp" src="img/banner.jpg">
+    <div class="row espacioArriba espacioAbajo wow bounceInUp">
+        <div class="col-lg-12 text-center fuenteAzul">
+        <h2><span class="glyphicon glyphicon-list-alt"></span> Objetivos de la Red de Jóvenes por el Agua</h2><br>
+        <h3>Objetivo General:</h3><br>
+        <h4>Fomentar la participación juvenil centroamericana en la GIRH.</h4><br>
+        <h3>Objetivos Especificos:</h3><br>
+        <h4>OE1: Articular actores relevantes y aliados potenciales para trabajar en conjunto por la GIRH promoviendo la participación activa de <br>las juventudes en espacios políticos.<br><br>
+        OE2: Fomentar la formación y el intercambio de experiencias, avances locales, nacionales y regionales con integrantes de la Red de Jóvenes por el Agua<br> en temas ambientales, económicos, políticos y sociales contribuyendo así a la construcción de conocimiento alrededor de la gestión del agua.<br><br>
+        OE3: Sensibilizar y generar cambios positivos en el manejo de recursos hídricos involucrando a jóvenes en procesos de participación e<br> incidencia ciudadana en la conservación del recurso.
+        </h4>
+        </div>
+    </div>
+    <div class="row bloqueAzul">
+        <div class="container espacioArriba espacioAbajo">
+            <div class="col-lg-12 fuenteBlanca text-center wow bounceInRight">
+            <h2><span class="glyphicon glyphicon-leaf"></span> Áreas Temáticas</h2><br></div>
+            <div class="col-lg-6 fuenteBlanca text-center wow bounceInLeft">
+                <h4>Cambio climático<br><br>
+                    Gobernanza Ambiental<br><br>
+                    Seguridad Alimentaria<br><br>
+                    Educación y liderazgo</h4>
+            </div>
+            <div class="col-lg-6 fuenteBlanca text-center wow bounceInRight">
+                <h4>Ecosistemas<br><br>
+                    Ciencia y Tecnología<br><br>
+                    Gestión Comunitaria</h4>
+            </div>
+        </div>
+    </div>
+    <br><br>
+@endsection
--- resources/views/voluntariado.blade.php
+++ resources/views/voluntariado.blade.php
@@ -1,48 +1,35 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>RJxA</title>
-        <!-- Fonts -->
-
-    </head>
-    <body>
-    @extends('layouts.menu')
-    @section('menu')
-    @parent
+@extends('layouts.menu')
+@section('menu')
+@parent
     <div class="row espacioAbajo wow bounceInUp">
         <img class="imgCompleta" src="img/voluntario.png">
-        <div class="prueba fuenteBlanca">
+    <div class="prueba fuenteBlanca">
         <h1>Perfil del Voluntario</h1>
-        </div>
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10 text-center fuenteAzul">
+    </div>
+    <div class="col-lg-1"></div>
+    <div class="col-lg-10 text-center fuenteAzul">
         <h3>El/la Voluntario(a) de la red nacional, debe participar en las acciones desarrolladas por la red con miras a contribuir a la Gestión Integral de los  Recursos  Hídricos  (GIRH)  de  su  país  y,  en  la  medida  de  lo  posible,  de  la región centroamericana. El  rol  como  Voluntario(a)  es  una experiencia  que  involucra responsabilidades  pero  es extremadamente gratificante.  </h3><br>
-        </div>
-        <div class="col-lg-1"></div>
     </div>
-    <div class="row bloqueAzul">
-        <div class="container espacioArriba espacioAbajo">
-            <div class="col-lg-5 fuenteBlanca text-center wow bounceInLeft">
-                <h2>¿De qué debe ser capaz un voluntario?</h2><br>
-                <h4><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;De escuchar, empatizar y trabajar con los voluntarios miembros de la red de Jóvenes por el  Agua,  con  la junta  directiva de  su  país y  con  demás voluntarios de  las organizaciones aliadas.<br><br><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Contar con habilidades  de  liderazgo aprendidas en otros  trabajos  voluntarios,  en  su  perfil académico/profesional o experiencia laboral.<br><br>
-                <span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Colaborar en el proceso de identificación e implementación de soluciones prácticas y eficaces para contribuir  al  cumplimiento de las  metas nacionales  o  superar  los problemas que surjan en el camino.</h4>
-            </div>
-            <div class="col-lg-2"></div>
-            <div class="col-lg-5 fuenteBlanca text-center wow bounceInRight">
-                <h2>¿Cuáles requisitos debe cumplir?</h2><br>
-                <h4><span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Joven entre 18 y 35 años.<br><br>
-                    <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Nacionalidad de cualquiera de los países centroamericanos.<br><br>
-                    <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Deseos de trabajar en pro de la GIRH y con grupos de personas de diversos orígenes, creencias y culturas.<br><br>
-                    <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Enfoque positivo basándose en expectativas reales.<br><br>
-                    <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Adaptabilidad a situaciones nuevas y ser capaz de trabajar bajo presión
-                </h4>
+            <div class="col-lg-1"></div>
+        </div>
+        <div class="row bloqueAzul">
+            <div class="container espacioArriba espacioAbajo">
+                <div class="col-lg-5 fuenteBlanca text-center wow bounceInLeft">
+                    <h2>¿De qué debe ser capaz un voluntario?</h2><br>
+                    <h4><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;De escuchar, empatizar y trabajar con los voluntarios miembros de la red de Jóvenes por el  Agua,  con  la junta  directiva de  su  país y  con  demás voluntarios de  las organizaciones aliadas.<br><br><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Contar con habilidades  de  liderazgo aprendidas en otros  trabajos  voluntarios,  en  su  perfil académico/profesional o experiencia laboral.<br><br>
+                    <span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Colaborar en el proceso de identificación e implementación de soluciones prácticas y eficaces para contribuir  al  cumplimiento de las  metas nacionales  o  superar  los problemas que surjan en el camino.</h4>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-5 fuenteBlanca text-center wow bounceInRight">
+                    <h2>¿Cuáles requisitos debe cumplir?</h2><br>
+                    <h4><span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Joven entre 18 y 35 años.<br><br>
+                        <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Nacionalidad de cualquiera de los países centroamericanos.<br><br>
+                        <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Deseos de trabajar en pro de la GIRH y con grupos de personas de diversos orígenes, creencias y culturas.<br><br>
+                        <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Enfoque positivo basándose en expectativas reales.<br><br>
+                        <span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;Adaptabilidad a situaciones nuevas y ser capaz de trabajar bajo presión.
+                    </h4>
+                </div>
             </div>
         </div>
-    </div>
-        <div class="row"><br><br><br></div>
-    @endsection
-    </body>
-</html>
+@endsection
+  
